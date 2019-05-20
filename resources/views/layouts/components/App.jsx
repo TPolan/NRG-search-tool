20f7512213bc2e8f53a19674b814544a1e4181bc
TPolan <t.poly@seznam.cz>
2019-05-20 16:00:01 +0200

    preset react to laravel

---
+++ resources/views/layouts/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+    return(
+        <div>
+            <h1>HELLO WORLD</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App
